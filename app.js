95a2e02ee74fcd9ce0d4e817f4267d2eef02cad2
Codioz <el-shadow@hotmail.fr>
2019-02-17 15:19:16 +0800

    Adding the hashing

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const request = require("request");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 const port = 3000;
 
 var app = express();
@@ -27,8 +27,6 @@ const userSchema = new mongoose.Schema ({
   }
 });
 
-const secret = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -49,7 +47,7 @@ app.get("/register", (req, res) => {
 app.post("/register", function(req, res){
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save((err) => {
     if(err){
@@ -64,7 +62,7 @@ app.post("/register", function(req, res){
 app.post("/login", function(req, res){
 
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
 
